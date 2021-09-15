ff3af23b99d04cfe9fd147d9c7eb0eacb5315d93
Jonathan Martel <jonathmartel@gmail.com>
2021-09-15 15:05:12 -0400

    Avec le chargement dynamique des vues

--- js/Affichage.mjs
+++ js/Affichage.mjs
@@ -1,5 +1,29 @@
 
 export default class Affichage {
+    static AfficherEnregistrer(){
+        const template = document.querySelector("#tmplEnregistrer").innerHTML;
+        const render = Mustache.render(template, {test:"allo"});
+        document.querySelector("main").innerHTML = render;
+    }
+
+    static AfficherTemplate(sTmpl, oData, domNoeud){
+        const rendu = Mustache.render(sTmpl, oData);
+        domNoeud.innerHTML = rendu;
+    }
     
-    
+    static chargementTemplate(aTemplates){
+        let htmlTemplate = [];
+        aTemplates.forEach(uneRoute=>{
+            htmlTemplate.push(fetch("./vues/"+uneRoute.fichier)
+                .then(reponse=>reponse.text())
+                .then(template=> { 
+                                    uneRoute.tmpl = template; 
+                                    console.log(uneRoute)
+                                }));
+        });
+
+        console.log(htmlTemplate);
+        return Promise.all(htmlTemplate);
+    }
+
 }
--- js/main.mjs
+++ js/main.mjs
@@ -1,19 +1,37 @@
+import Affichage from './Affichage.mjs';
 import _App from './App.mjs';
 import Tache from './Tache.mjs';
 
 (function(){
     let App = {};
+
+    const aRoutes = [
+        {fichier:"enregistrer.html", tmpl : ""}, 
+        {fichier:"connecter.html", tmpl : ""}, 
+        {fichier:"tache.html", tmpl : ""}, 
+        {fichier:"ajouter.html", tmpl : ""}, 
+    ];
+
     document.addEventListener("DOMContentLoaded", ()=>{
         //App = new _App();
         //Tache.setUsager({}, (data)=>{console.log(data, "nouvel usager")});
         /*Tache.setUsager({})
             .then((data)=>{console.log("nouvel usager", data)});*/
-        Tache.logUsager()
+        /*Tache.logUsager()
             .then(()=>{
                 console.log("connecté");
                 Tache.delUsager()
-            });
+            });*/
+
         //setTimeout(function(){Tache.delUsager()}, 5000);
         //Tache.delUsager();
-    })
+
+        //const tmpl = document.querySelector("#tmplEnregistrer").innerHTML;
+        const noeud = document.querySelector("main");
+        //Affichage.AfficherTemplate(tmpl,{}, noeud);
+        
+        Affichage.chargementTemplate(aRoutes)
+        .then(
+            ()=>Affichage.AfficherTemplate(aRoutes[1].tmpl,{}, noeud));        
+        });
 })()
